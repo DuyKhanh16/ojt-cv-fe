65b90a4fe07afbfb77e12d2ed2ab49b7692b0d68
DuyKhanh16 <duykhanh16121993@gmail.com>
2024-04-08 14:49:01 +0700

    create listJob

--- src/pages/home/main/Main.jsx
+++ src/pages/home/main/Main.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import Footer from '../../../components/footer/Footer'
+import JobList from '../homeUser/jobList/JobList'
 
 export default function Main() {
   return (
     <>
+      <JobList></JobList>
       <Footer></Footer>
     </>
   )
