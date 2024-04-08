0f177e79bedad78632abdcac23eea1fa3f19873e
hoang <khuongdanhhoang123@gmail.com>
2024-04-09 00:13:57 +0700

    resposive_demo-header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,13 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./Header.scss"
 import { NavLink } from 'react-router-dom'
 import phone from "../../assets/images/main/PhoneCall.png"
 import american from "../../assets/images/main/image123.png"
 export default function Header() {
+  const [isOpen, setIsOpen] = useState(false)
+
+  const open = () => {
+    console.log(isOpen)
+    setIsOpen(!isOpen)
+  }
   return (
     <>
       <div className="header">
-        <div className='header__navbar'>
+      <div className='header__navbar__menu' onClick={open} >
+          <span class="material-symbols-outlined " >menu</span>
+          </div>
+        <div className="header__navbar" style={{ height:isOpen?"300px ":"0px" }}>
           <NavLink to="/" className="navbar_item">
             Trang chủ
           </NavLink>
@@ -21,16 +30,16 @@ export default function Header() {
             Customer Supports
           </NavLink>
         </div>
-        <div className='header__contact'>
-          <div className='header__contact__phone'>
+        <div className="header__contact">
+          <div className="header__contact__phone">
             <img src={phone} alt="" height={24} width={24} />
             <p> +84 123 456 789</p>
           </div>
-          <div className='header__language'>
-             <img src={american} alt="" />
-             <select>
-                <option value="">English</option>
-             </select>
+          <div className="header__language">
+            <img src={american} alt="" />
+            <select>
+              <option value="">English</option>
+            </select>
           </div>
         </div>
       </div>
