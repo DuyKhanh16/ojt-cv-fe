ed2c3fe7ce710c4472cd8512dc9b3c7986fc77ee
Kingsman <hoangbaohs22@gmail.com>
2024-04-15 20:28:27 +0700

    update private

--- src/config/private.axios.js
+++ src/config/private.axios.js
@@ -25,14 +25,14 @@
 // export default privateAxios;
 import axios from "axios";
 
-const baseURL = "http://localhost:3000/";
+const baseURL = "http://localhost:3000";
 
 const privateAxios = axios.create({
   baseURL,
 });
 
 privateAxios.interceptors.request.use((config) => {
-  const jwtTokent = localStorage.getItem("token");
+  const jwtTokent = JSON.parse(localStorage.getItem("token"));
 
   return {
     ...config,
