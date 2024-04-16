52fbbcf41fb8e2ad74a56daaf3eb08800ce48fcf
hoang <khuongdanhhoang123@gmail.com>
2024-04-16 19:43:09 +0700

    create logic update info candiadte

--- src/components/modal/updateInforUser/Certificate.jsx
+++ src/components/modal/updateInforUser/Certificate.jsx
@@ -1,8 +1,36 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './formModal.scss'
 import './Certificate.scss'
+import privateAxios from '../../../config/private.axios';
+import { notification } from 'antd';
 
 export default function Certificate({isOpen,close}) {
+  const [user, setUser] = useState({});
+  useEffect(() => {
+    const getUser = privateAxios.get("api/v2/candidates/getInfor")
+    getUser.then((res) => {
+      console.log("API response data:", res.data.data)
+      setUser({...user,candidate_id:res.data.data.id});
+
+    }) 
+  }, [])
+const changeValue = (e) => {
+  setUser({...user,[e.target.name]:e.target.value})
+}
+const updateCertificate = async () => {
+  console.log(user)
+  try {
+    const create = await privateAxios.post(`api/v2/candidate/createCertificate`,user)
+    notification.success({
+      message: create.data.message,
+    })
+    close()
+  } catch (error) {
+    notification.error({
+      message: error.response.data.message,
+    })
+  }
+}
   return (
     <>
     <div style={{display:isOpen?'block':'none'}}>
@@ -12,33 +40,33 @@ export default function Certificate({isOpen,close}) {
         <div className="updateInforUser__table certificate">
             <div>
               <p>Tên chứng chi</p>
-              <input type="text" placeholder='ABCde' />
+              <input type="text" placeholder='ABCde' name='name' onChange={(e)=>changeValue(e)}/>
             </div>
             <div>
               <p>Tổ chức</p>
-              <input type="text" placeholder='ABCde' />
+              <input type="text" placeholder='ABCde' name="organization" onChange={(e)=>changeValue(e)} />
             </div>
 
             <div className='certificateTime'>
               <p>Thời gian</p>
               <div className='timeSame'>
                 <label htmlFor="">Start Date</label>
-                <input type="date" className='date'/>
+                <input type="date" className='date' name='start_at' onChange={(e)=>changeValue(e)}/>
               </div>
               <p>To</p>
               <div className='timeSame'>
                 <label htmlFor="">End Date</label>
-                <input type="date" className='date'/>
+                <input type="date" className='date' name='end_at' onChange={(e)=>changeValue(e)}/>
               </div>
             </div>
 
             <div>
               <p>Mô tả thêm</p>
-              <textarea name="" id="" cols="30" rows="10" placeholder='ABCde'></textarea>
+              <textarea name="" id="" cols="30" rows="10" placeholder='ABCde' onChange={(e)=>setUser({...user,info:e.target.value})}></textarea>
             </div>
         </div>
         <div className="updateInforUser__button">
-          <button>Cập nhập</button>
+          <button onClick={updateCertificate}>Cập nhập</button>
           <button className='updateInforUser__button__cancel' onClick={()=>close()}>Hủy Bỏ</button>
         </div>
         </div> 
