c1e083e04b789a71c634f00238b38b4df45856a7
thuan <tranhuuthuan05021996@gmail.com>
2024-04-19 17:22:40 +0700

    admin

--- src/pages/home/admin/allCompanyAdmin/AllCompanyAdmin.jsx
+++ src/pages/home/admin/allCompanyAdmin/AllCompanyAdmin.jsx
@@ -4,9 +4,51 @@ import "./AllCompanyAdmin.scss";
 import AdminSearch from "../../../../components/adminSearch/AdminSearch";
 import { useNavigate } from "react-router";
 import publicAxios from "../../../../config/pulic.axios";
+import { Switch , Modal } from 'antd';
 export default function AllCompanyAdmin() {
   const [allCompany, setAllCompany] = useState([]);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [listbrand, setListbrand] = useState([]);
+  const [isChecked, setIsChecked] = useState(false);
+  const [infoCompany,SetinfoCompany] = useState({
+    title : "",
+    phone: "",
+    logo:"",
+    size:"",
+    typeCompany:"",
+    address : "",
+    email : "",
+   })
+
   const navigate = useNavigate();
+
+  const showModal = (item) => {
+    // console.log(item)
+    SetinfoCompany(
+      {
+        title: item.name  ,
+        phone : item.phone,
+        logo : item.logo,
+        email : item.email_company,
+        size : item.size,
+        address : item.address_company[0].address,
+        typeCompany: item.typeCompany_id.name
+      }
+    )
+    setListbrand(item.address_company)
+    setIsModalOpen(true);
+  };
+
+  console.log(infoCompany)
+  const handleOk = () => {
+    setIsModalOpen(false);
+  };
+
+  const handleCancel = () => {
+    setIsModalOpen(false);
+  };
+
+
   const getAllCompany = async () => {
     try {
       const res = await publicAxios.get("/api/v2/companies/getAll");
@@ -20,10 +62,56 @@ export default function AllCompanyAdmin() {
     getAllCompany();
   }, []);
 
+  // thay đôi trạng thái công ty
+  // Hàm xử lý sự kiện thay đổi của Switch, nhận vào dữ liệu data và sự kiện event
+  const handleChange = (data) => (checked) => {
+    console.log("Data:", data); // In ra dữ liệu được truyền vào
+    console.log("Switch checked:", checked); // In ra trạng thái của Switch
+
+    // Thực hiện các xử lý khác tại đây nếu cần
+    setIsChecked(checked); // Cập nhật trạng thái của Switch
+  };
+
 
   return (
     <>
       <div className="allCompanyAdmin__formsearch">
+      
+      <Modal 
+      width={800}
+      title="Thông tin Doanh nghiệp" open={isModalOpen} onOk={handleOk} onCancel={handleCancel}>
+      <div style={{display:"flex",gap:"10px",marginBottom:"10px"}} >
+        <div>Tên công ty :</div>
+        <div style={{fontWeight:"bold",fontSize:"18px"}}> {infoCompany.title}</div>
+      </div>
+      <div style={{display:"flex",gap:"10px",marginBottom:"10px"}} >
+        <div>Số điện thoại liên hệ :</div>
+        <div  style={{fontWeight:"bold",fontSize:"16px"}}> {infoCompany.phone}</div>
+      </div>
+      <div style={{marginBottom:"50px"}}>
+        <div>Logo Công ty :</div>
+        <div > <img 
+        style={{width:"150px",height:"150px",borderRadius:"50%",marginLeft:"200px"}}
+        src={infoCompany.logo}></img></div>
+      </div>
+      <div style={{marginBottom:"10px",display:"flex",gap:"10px"}} >
+        <div>Địa chỉ :</div>
+        <div style={{fontWeight:"bold",fontSize:"18px"}}> {infoCompany.address}</div>
+      </div>
+    <div style={{marginBottom:"10px",display:"flex",gap:"10px"}} >
+      <div>Số lượng nhân viên :</div>
+      <div style={{fontWeight:"bold",fontSize:"18px"}}> {infoCompany.size} Người</div>
+    </div>
+      <div style={{marginBottom:"10px",display:"flex",gap:"10px"}} >
+        <div>Địa chi email :</div>
+        <div style={{fontWeight:"bold",fontSize:"18px"}}> {infoCompany.email} </div>
+      </div>
+    <div style={{marginBottom:"10px",display:"flex",gap:"10px"}}>
+      <div>Dạng công ty :</div>
+      <div style={{fontWeight:"bold",fontSize:"18px"}}> {infoCompany.typeCompany}</div>
+    </div>
+  
+    </Modal>
       <AdminSearch></AdminSearch>
       </div>
       <div className="allCompanyAdmin__container">
@@ -59,19 +147,13 @@ export default function AllCompanyAdmin() {
                   <p>{item?.phone}</p>
                 </div>
                 <div className="allCompanyAdmin__content__bodyTable__item__active column">
-                  <label class="allCompanyAdmin__content__bodyTable__item__active__btn">
-                    <input
-                      type="checkbox"
-                      id="toggle-input"
-                      class="toggle-input"
-                    />
-                    <div class="toggle-bar">
-                      <div class="toggle-spin"></div>
-                    </div>
-                    
-                  </label>
+                <Switch defaultChecked
+                // checked={item.status === 1 ? true : false}
+                onChange={handleChange(item.id)} />
                 </div>
-                <div className="allCompanyAdmin__content__bodyTable__item__description column">
+                <div
+                onClick={()=>showModal(item)}
+                 className="allCompanyAdmin__content__bodyTable__item__description column">
                   <p>Xem</p>
                 </div>
              </div>
--- src/pages/home/admin/allJobAdmin/AllJobAdmin.jsx
+++ src/pages/home/admin/allJobAdmin/AllJobAdmin.jsx
@@ -4,26 +4,138 @@ import "./AllJobAdmin.scss";
 import AdminSearch from "../../../../components/adminSearch/AdminSearch";
 import { useNavigate } from "react-router";
 import publicAxios from "../../../../config/pulic.axios";
+import { Switch, Modal, notification } from 'antd';
+import axios from "axios";
+
 export default function AllJobAdmin() {
   const [allNewJob, setNewJob] = useState([]);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [flag, setflag] = useState(false);  
+  const [isChecked, setIsChecked] = useState(false);
+  const [infoJob, setInfoJob] = useState({
+    name: "",
+    description: "",
+    salary: "",
+    address: "",
+    typejob_id: "",
+    requirements: "",
+    created_at: "",
+    expire_at: "",
+    nameCompany: "",
+  });
   const navigate = useNavigate();
+
+  const showModal = (item) => {
+    console.log(item);
+    setInfoJob({
+      name: item.title,
+      salary: item.salary,
+      address: item.address_company.address,
+      typejob_id: item.types_jobs[0].typejob.name,
+      requirements: item.requirements,
+      description: item.description,
+      created_at: item.created_at,
+      expire_at: item.expire_at,
+      nameCompany: item.company.name,
+    });
+    setIsModalOpen(true);
+  };
+  // console.log(infoJob);
+  const handleOk = () => {
+    setIsModalOpen(false);
+  };
+
+  const handleCancel = () => {
+    setIsModalOpen(false);
+  };
+ const onChange =  async (checked, event, job) => {
+    console.log(checked, event, job);
+    setIsChecked(checked);
+    const id = job.id;  
+    try {
+      const res = await axios.patch(`http://localhost:3000/api/v2/jobs/updatestatus/${id}?status=${checked ? 0 : 1}`, )
+      console.log(res);
+      notification.success({ message: "Đã thay đổi trạng thái", placement: 'bottomRight', duration: 2 });
+      setflag(!flag);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   const getAllNewJob = async () => {
     try {
-      const res = await publicAxios.get("/api/v2/jobs/getNewJobs");
+      const res = await publicAxios.get("/api/v2/jobs/getJobsforadmin");
       setNewJob(res.data.data);
     } catch (error) {
       console.log(error);
     }
   };
 
-  console.log(allNewJob);
+  // console.log(allNewJob);
   useEffect(() => {
     getAllNewJob();
-  }, []);
+  }, [flag]);
+  console.log(allNewJob);
   return (
     <>
       <div className="allJobAdmin__formsearch">
+        <Modal
+          title="Thông tin công việc"
+          open={isModalOpen}
+          onOk={handleOk}
+          onCancel={handleCancel}>
+          <div style={{ display: "flex" ,marginTop:"10px",marginBottom:"10px"}}>
+            <div>Tên công ty  </div>
+            <div>: </div>
+            <div style={{ marginLeft: "10px", fontWeight:"bold" }}>{infoJob.nameCompany}</div>
+          </div>
+          <div style={{ display: "flex" ,marginTop:"10px",marginBottom:"10px"}}>
+            <div>Tên Jobs </div>
+            <div>: </div>
+            <div style={{ marginLeft: "10px",fontWeight:"bold" }}>{infoJob.name}</div>
+          </div>
+          <div style={{ display: "flex" ,marginTop:"10px",marginBottom:"10px"}}>
+            <div>Mô tả </div>
+            <div>: </div>
+            </div>
+            <div style={{ fontWeight:"bold" }}>{infoJob.description}</div>
+          <div style={{ display: "flex" ,marginTop:"10px",marginBottom:"10px"}}>
+            <div>Yêu cầu công việc </div>
+            <div>: </div>
+          </div>
+          <div style={{ fontWeight:"bold" }}>{infoJob.requirements}</div>
+
+          <div style={{ display: "flex" ,marginTop:"10px",marginBottom:"10px"}}>
+            <div>Mức lương </div>
+            <div>: </div>
+            <div style={{ fontWeight:"bold",marginLeft: "10px", }}>{infoJob.salary}</div>
+          </div>
+          <div style={{ display: "flex" ,marginTop:"10px",marginBottom:"10px"}}>
+            <div>Địa chỉ </div>
+            <div>: </div>
+            </div>
+            <div style={{ fontWeight:"bold" }}>{infoJob.address}</div>
+          <div style={{ display: "flex" ,marginTop:"10px",marginBottom:"10px"}} >
+            <div>Thời gian làm việc </div>
+            <div>: </div>
+            <div style={{ fontWeight:"bold",marginLeft: "10px",  }}>{infoJob.typejob_id}</div>
+          </div>
+          <div style={{ display: "flex" ,marginTop:"10px",marginBottom:"10px",alignItems:"center",justifyContent:"space-between"}}>
+            <div style={{marginRight:"10px"}}>Thời gian tuyển dụng :</div>
+            <div style={{ marginLeft: "10px",borderBottom:"1px solid red",paddingBottom:"5px" }}>
+              <div>Ngày bắt đầu </div>
+              <div> {infoJob.created_at.toString().slice(0, 10)}</div>
+            </div>
+            <div>to</div>
+            <div style={{ marginLeft: "10px",borderBottom:"1px solid red",paddingBottom:"5px" }}>
+              <div>Ngày kết thúc </div>
+              <div> {infoJob.expire_at}</div>
+            </div>
+           
+          </div>
+          
+
+        </Modal>
         <AdminSearch></AdminSearch>
       </div>
       <div className="allJobAdmin__container">
@@ -52,11 +164,11 @@ export default function AllJobAdmin() {
             </div>
           </div>
           <div className="allJobAdmin__content__bodyTable">
-            {allNewJob.map((item,index) => {
+            {allNewJob.map((item, index) => {
               return (
                 <div className="allJobAdmin__content__bodyTable__item" key={item?.id}>
                   <div className="allJobAdmin__content__headerTable__stt column">
-                    <p>{index+1}</p>
+                    <p>{index + 1}</p>
                   </div>
                   <div className="allJobAdmin__content__headerTable__namejob column">
                     <p>{item?.title} </p>
@@ -75,22 +187,18 @@ export default function AllJobAdmin() {
                     <div className="allJobAdmin__content__headerTable__salary__start ">
                       {item?.salary}
                     </div>
-                    
+
                   </div>
                   <div className="allJobAdmin__content__headerTable__active column">
-                    <label class="allJobAdmin__content__bodyTable__item__active__btn">
-                      <input
-                        type="checkbox"
-                        id="toggle-input"
-                        class="toggle-input"
+                    <Switch defaultChecked
+                      // checked={item.status === 1 ? true : false}
+                      onChange={(checked, event) => onChange(checked, event, item)} // Truyền thông tin về công việc vào hàm onChange
                       />
-                      <div class="toggle-bar">
-                        <div class="toggle-spin"></div>
-                      </div>
-                    </label>
                   </div>
-                  <div className="allJobAdmin__content__headerTable__description column">
-                    <p>Truy cập</p>
+                  <div
+                    onClick={() => showModal(item)}
+                    className="allJobAdmin__content__headerTable__description column">
+                    <p>Xem</p>
                   </div>
                 </div>
               );
