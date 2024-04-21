ca49c9eec6aa92f6180a105d80ea5602efa59af0
thuan <tranhuuthuan05021996@gmail.com>
2024-04-21 13:57:19 +0700

    nhúng iframer-map

--- src/pages/home/homeBusiness/updateInforBusiness/UpdateInforBusiness.jsx
+++ src/pages/home/homeBusiness/updateInforBusiness/UpdateInforBusiness.jsx
@@ -823,8 +823,17 @@ export default function UpdateInforBusiness() {
               <p>
                 <i class="fa-regular fa-map"></i> Xem trên Maps
               </p>
-              <img src={map} alt="" />
-            </div>
+              <iframe
+              title="Google Maps"
+              width="400"
+              height="350"
+              frameborder="0" 
+              style={{ border: 0 ,}}
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d7456.362334988663!2d106.68267485!3d20.8647468!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x314a7af131094315%3A0x9a9d9e52c6ccc37b!2zTWluaCBLaGFpLCBI4buTbmcgQsOgbmcsIEjhuqNpIFBow7JuZw!5e0!3m2!1svi!2s!4v1713681761804!5m2!1svi!2s"
+              allowfullscreen=""
+              aria-hidden="false"
+              tabindex="0"
+            ></iframe>            </div>
             <div
               className="user-companyView-orther-social-container"
               style={{
