292d2010e7ed2c93861a7fa2ec9a5c8367b17abf
Kingsman <hoangbaohs22@gmail.com>
2024-04-25 10:16:11 +0700

    done api candidate&&job

--- src/apis/patchApi.js
+++ src/apis/patchApi.js
@@ -3,7 +3,34 @@ export const API_REGISTER = "/auth/register";
 //HET AUTH
 
 //CANDIDATE
-export const API_CANDIDATE_DELETECANDIDATE = "/candidate/deleteCertificate";
+export const API_CANDIDATE_GETALL = "/candidates/getAll";
+export const API_CANDIDATE_GETINFOR = "/candidates/getInfor";
+export const API_CANDIDATE_GETALLINFORMATION = "/candidates/getAllInformation";
+export const API_CANDIDATE_UPDATEABOUTME = "/candidates/updateAboutMe";
+export const API_CANDIDATE_UPDATEINFORCANDIDATE = "/candidates/updateInfoCandidate";
+export const API_CANDIDATE_GETINFORCANDIDATE_BYID = "/candidates/getInforCandidateById";
+export const API_CANDIDATE_SEARCHCANDIDATE = "/candidates/searchCandidate";
+export const API_CANDIDATE_GETALLPROJECT = "/candidate/getAllProject";
+export const API_CANDIDATE_CREATEPROJECT = "/candidate/createProject";
+export const API_CANDIDATE_UPDATEPROJECT_BYID = "/candidate/updateProject";
+export const API_CANDIDATE_DELETEPROJECT_BYID = "/candidate/deleteProject";
+export const API_CANDIDATE_GETALLSKILLS = "/candidate/getAllSkills";
+export const API_CANDIDATE_CREATESKILL = "/candidate/createSkill";
+export const API_CANDIDATE_UPDATESKILL_BYID = "/candidate/updateSkill";
+export const API_CANDIDATE_DELETESKILL_BYID = "/candidate/deleteSkill";
+export const API_CANDIDATE_GETALLEDUCATIONCANDIDATE = "/candidate/getAllEducationCandidate";
+export const API_CANDIDATE_CREATEDUCATION = "/candidate/createEducation";
+export const API_CANDIDATE_UPDATEEDUCATION_BYID = "/candidate/updateEducation";
+export const API_CANDIDATE_DELETEEDUCATION_BYID = "/candidate/deleteEducation";
+export const API_CANDIDATE_GETALLEXPERIENCE = "/candidate/getAllExperience";
+export const API_CANDIDATE_CREATEEXPERIENCE = "/candidate/createExperience";
+export const API_CANDIDATE_UPDATEEXPERIENCE_BYID = "/candidate/updateExperience";
+export const API_CANDIDATE_DELETEEXPERIENCE_BYID = "/candidate/deleteExperience";
+export const API_CANDIDATE_GETALLCERTIFICATE = "/candidate/getAllCertificate";
+export const API_CANDIDATE_CREATECERTIFICATE = "/candidate/createCertificate";
+export const API_CANDIDATE_UPDATECERTIFICATE_BYID = "/candidate/updateCertificate";
+export const API_CANDIDATE_DELETECERTIFICATE_BYID = "/candidate/deleteCertificate";
+
 // HET CANDIDATE
 
 //COMPANY
@@ -13,10 +40,39 @@ export const API_COMPANY_GETBRAND_BYID = "/companies/getbrandcompany";
 export const API_COMPANY_UPDATEINFOR_BYID = "/companies/update-info";
 //HET COMPANY
 
+//JOB
+export const API_JOB_GETLIVEJOB = "/jobs/getLiveJobs";
+export const API_JOB_GETJOBFORADMIN = "/jobs/getJobsforadmin";
+export const API_JOB_GETNEWJOB = "/jobs/getNewJobs";
+export const API_JOB_GETJOBFORCOMPANY = "/jobs/getJobsForCompany";
+export const API_JOB_COMPANY_BYID = "/jobs/company";
+export const API_JOB_CREATE_BYID = "/jobs/create";
+export const API_JOB_EDIT_BYID = "/jobs/edit";
+export const API_JOB_GETCANDIDATE_BYIDJOB = "/jobs/getCandidatesbyIdJob";
+export const API_JOB_GETCANDIDATEAPPLYING = "/jobs/getCandidatesApplying";
+export const API_JOB_DETAIL_BYID = "/jobs/detail";
+export const API_JOB_UPDATESTATUS_BYID = "/jobs/updateStatus";
+export const API_JOB_APPLYJOB = "/jobs/applyJob";
+export const API_JOB_GETJOBAPPLIEDCANDIDATES = "/jobs/getJobAppliedCandidates";
+export const API_JOB_CANCELCANDIDATE_BYID = "/jobs/cancelCandidate";
+export const API_JOB_SEARCHJOB = "/jobs/searchJob";
+export const API_JOB_UPDATEINTERVIEWDATE_BYID = "/jobs/update-interview-date";
+export const API_JOB_GETJOBAPPLIEDCANDIDATES_BYID = "/jobs/getJobAppliedCandidatesById";
+
+//HET JOB
+export const API_TYPEJOB_GETALL = "/typejob/getAll";
+export const API_LEVELJOB_GETALL = "/leveljob/getAll";
+
+//TYPEJOB   
+//HET TYPEJOB
+
 //ADMIN
 //HET ADMIN
 
 //ULTIS
 export const API_MAIL_SEND = "/mail/send";
+export const API_ACCOUNT_UPDATE_BYID = "/account/update";
 export const API_SALARY_GETALL = "/salary/getAll";
+
+
 //HET ULTIS
